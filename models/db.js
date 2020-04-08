5dac17ea0d4f284e2fcf74278666dc6113868aa8
suvang <suvang08@gmail.com>
2020-04-08 14:41:18 +0530

    changed server

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://heroku_vrdfzbr5:gqj3suuo5vq1i11clr8vgv7ai3@ds117846.mlab.com:17846/heroku_vrdfzbr5'
+mongoose.connect(process.env.MONGODB_URI
 , { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
