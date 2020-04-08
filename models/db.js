5e4e8bf9e0d16785fed0cfffb9681c169ee04418
suvang <suvang08@gmail.com>
2020-04-08 19:46:39 +0530

    changed server

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/EmployeeDB'
 , { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
